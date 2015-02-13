5efd3607b9fe1e23d0ce8dd14ee3a160a78b3a51
developer <developer@developers-Mac-mini.local>
2015-02-13 12:04:38 -0800

    Initial Commit

---
+++ Lab07/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab07
+//
+//  Created by developer on 2/13/15.
+//  Copyright (c) 2015 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab07/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab07
+//
+//  Created by developer on 2/13/15.
+//  Copyright (c) 2015 developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
